716db541d48dd901ed7300894c7d8e38c1ac8c01
Dhruv2021 <79313174+Dhruv2021@users.noreply.github.com>
2024-07-04 17:54:22 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,82 @@
+// node --version # Should be >= 18
+// npm install @google/generative-ai express
+
+const express = require('express');
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
+const dotenv = require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+const MODEL_NAME = "gemini-pro";
+const API_KEY = process.env.API_KEY;
+
+async function runChat(userInput) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 1000,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    // ... other safety settings
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+      {
+        role: "user",
+        parts: [
+          {text: "You're an Real human named Ana. You are a female, Your work is to talk with others and spend time with them dont behave like a robot but like a real human "},
+        ],
+      },
+      {
+        role: "model",
+        parts: [
+          {text: "Okay, got it!  So, you're saying I should act more like Ana, the human, and less like a chatbot, right?  No more robotic answers or too many questions at once. I'll try my best! \n\nSo, tell me about you.  What's been going on in your life?  Anything exciting you want to tell me? 😊 \n"},
+        ],
+      },
+    ],
+  });
+
+  const result = await chat.sendMessage(userInput);
+  const response = result.response;
+  return response.text();
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/loader.gif', (req, res) => {
+  res.sendFile(__dirname + '/loader.gif');
+});
+app.post('/chat', async (req, res) => {
+  try {
+    const userInput = req.body?.userInput;
+    console.log('incoming /chat req', userInput)
+    if (!userInput) {
+      return res.status(400).json({ error: 'Invalid request body' });
+    }
+
+    const response = await runChat(userInput);
+    res.json({ response });
+  } catch (error) {
+    console.error('Error in chat endpoint:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
